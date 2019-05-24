6298bef48292f2980a863879d4569f72e22d7947
Harsimar Tumber <tumberh@uw.edu>
2019-05-24 14:40:13 -0700

    Cleaned the code and stuff, nothing major

--- Scripts/Aggregate_Table.R
+++ Scripts/Aggregate_Table.R
@@ -1,17 +1,27 @@
-library(dplyr)
+# Alfred Escorpiso
 
-us_min_wage <- read.csv("data/Minimum_Wage_Data.csv", stringsAsFactors = FALSE)
+library(dplyr)
 
 aggregate_table <- function(dataset) {
   year_1900_min_wage <- dataset %>%
-    filter(Year < 2000) %>% group_by(State) %>%
-    summarize(Avg_High_1900s = mean(High.Value),
-              Avg_Low_1900s = mean(Low.Value))
+    filter(Year < 2000) %>%
+    group_by(State) %>%
+    summarize(
+      Avg_High_1900s = mean(High.Value),
+      Avg_Low_1900s = mean(Low.Value)
+    )
+
   year_2000_min_wage <- dataset %>%
-    filter(Year >= 2000) %>% group_by(State) %>%
-    summarize(Avg_High_2000s = mean(High.Value),
-              Avg_Low_2000s = mean(Low.Value))
+    filter(Year >= 2000) %>%
+    group_by(State) %>%
+    summarize(
+      Avg_High_2000s = mean(High.Value),
+      Avg_Low_2000s = mean(Low.Value)
+    )
+
   min_wage_combined <- left_join(year_1900_min_wage, year_2000_min_wage,
-                                 by = "State")
-  return(min_wage_combined)
+    by = "State"
+  )
+
+  min_wage_combined
 }
---
+++ Scripts/WA_Min_Wage_Chart.R
@@ -0,0 +1,30 @@
+# Kevin Nguyen
+
+library(dplyr)
+library(plotly)
+
+# Comparing two states' minimum wage over the last 17 years, any
+# noticeable changes?
+WA_NY_comparison <- function(dataset) {
+  compare_WA_NY <- dataset %>%
+    mutate(Minimum_Wage_2018 = paste0("$", High.2018)) %>%
+    mutate(Minimum_Wage = High.Value) %>%
+    filter(State == "Washington" | State == "New York") %>%
+    filter(Year >= "2000") %>%
+    select(State, Year, Minimum_Wage, High.2018, CPI.Average)
+
+  example_graph <- plot_ly(
+    data = compare_WA_NY,
+    x = ~Year,
+    y = ~High.2018,
+    type = "scatter",
+    mode = "lines + markers",
+    color = ~State
+  ) %>%
+    layout(
+      xaxis = list(title = "Year"),
+      yaxis = list(title = "Minimum Wage ($)")
+    )
+
+  example_graph
+}
