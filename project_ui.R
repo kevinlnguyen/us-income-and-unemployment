d6d178d38594a72db6b276cdd65dd621a507ade2
Alfred Escorpiso <alfiedawg.eaxii@yahoo.com>
2019-06-04 13:16:45 -0700

    Made changes to overview page

--- project_ui.R
+++ project_ui.R
@@ -1,13 +1,18 @@
 library("shiny")
 
 overview_page <- tabPanel("Overview",
-                          titlePanel("Overview of the Project"))
-interactive_page_one <- tabPanel("Interactive Part 1",
-                                 titlePanel("Interactive Page 1"))
-interactive_page_two <- tabPanel("Interactive Part 2",
-                                 titlePanel("Interactive Page 2"))
-interactive_page_three <- tabPanel("Interactive Part 3",
-                                   titlePanel("Interactive Page 3"))
+                          titlePanel("Overview of the Project"),
+                          mainPanel(
+                            h1("Minimum Wage Across United States"),
+                            p("Why are we interested about learning
+                              this type of dataset?")
+                          ))
+interact_page_one <- tabPanel("Interactive Part 1",
+                              titlePanel("Interactive Page 1"))
+interact_page_two <- tabPanel("Interactive Part 2",
+                              titlePanel("Interactive Page 2"))
+interact_page_three <- tabPanel("Interactive Part 3",
+                                titlePanel("Interactive Page 3"))
 summary_page <- tabPanel("Summary",
                          titlePanel("Summary of the Project"))
 project_ui <- navbarPage(
