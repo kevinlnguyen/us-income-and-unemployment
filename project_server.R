dc3544ced645d26117cf703b9df75105a2a2326e
Kevin Nguyen <kevinn97@uw.edu>
2019-06-04 23:13:00 -0700

    added Kevin's portion, new changes

--- project_server.R
+++ project_server.R
@@ -4,6 +4,7 @@ library("shiny")
 source("scripts/unemploy_bar_plot.R")
 source("scripts/WA_Min_Wage_Chart.R")
 source("scripts/rate_plot_over_time.R")
+source("scripts/Household_Income.R")
 
 ## Read in Minimum Wage Data
 unemployment <- read.csv("data/unemployement.csv", stringsAsFactors = FALSE)
@@ -47,5 +48,14 @@ project_server <- function(input, output){
     caption <- paste0("This data displays how the unemployment rate varies
                       across states during the year ", input$year_bar_input,
                       " to show the unemployment disparity across the US.")
+  })
+  
+  output$house_plot <- renderPlotly(
+    return(build_scatter(income, input$state))
+  )
+  output$household_plot_caption <- renderText({
+    house_cap <- paste0("This data displays the total median household income by", 
+                        input$state, "in regards to cities with different land and 
+                       geographic sizes.")
   })
 }
