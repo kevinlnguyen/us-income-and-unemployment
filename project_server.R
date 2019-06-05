9893d778893c6483e2a11db957347f6f46eb41ed
Kevin Nguyen <kevinn97@uw.edu>
2019-06-04 18:15:28 -0700

    add first chart stuff

--- project_server.R
+++ project_server.R
@@ -1,6 +1,21 @@
 library("dplyr")
 
+## Read in Minimum Wage Data 
 min_wage <- read.csv("data/Minimum_Wage_Data.csv", stringsAsFactors = FALSE)
+
+## Select certain columns of dataset
+wage_df <- read.csv("data/VZ_StateMinimumWage_Changes.csv", 
+                    stringsAsFactors = FALSE)
+wage_df <- wage_df %>% 
+  select(statename, year, month, day, VZ_mw) %>% 
+  filter(year != "2020") 
+
+#Display short description by state
+
+source("scripts/WA_Min_Wage_Chart.R")
+
 project_server <- function(input, output){
-  
+  output$plot <- renderPlotly({
+    return(build_scatter(wage_df, input$state_test))
+  })
 }
--- project_ui.R
+++ project_ui.R
@@ -1,4 +1,18 @@
 library("shiny")
+library(plotly)
+
+wage_df <- read.csv("data/VZ_StateMinimumWage_Changes.csv", 
+                    stringsAsFactors = FALSE)
+
+wage_df <- wage_df %>% 
+  select(statename, year, month, day, VZ_mw) %>% 
+  filter(year != "2020")
+
+states <- wage_df %>% 
+  select(statename) %>% 
+  unique()
+
+states <- unlist(states, use.names = FALSE)
 
 overview_page <- tabPanel("Overview",
                           titlePanel("Overview of the Project"),
@@ -7,8 +21,19 @@ overview_page <- tabPanel("Overview",
                             p("Why are we interested about learning
                               this type of dataset?")
                           ))
-interact_page_one <- tabPanel("Interactive Part 1",
-                              titlePanel("Interactive Page 1"))
+interact_page_one <- tabPanel("Reported Minimum Wage by State",
+                              sidebarLayout(sidebarPanel(
+                                selectInput(
+                                  "state_test",
+                                  label = "Select a State",
+                                  choices = states,
+                                  selected = "Washington"
+                                  )),
+                                  mainPanel(plotlyOutput("plot"))
+                                  ),
+                              p("This data shows us the fluctuation of minimum
+                                  wage by state, including whether or not 
+                                changes occurred."))
 interact_page_two <- tabPanel("Interactive Part 2",
                               titlePanel("Interactive Page 2"))
 interact_page_three <- tabPanel("Interactive Part 3",
@@ -18,8 +43,8 @@ summary_page <- tabPanel("Summary",
 project_ui <- navbarPage(
   "Minimum Wage in the United States",
   overview_page,
-  interactive_page_one,
-  interactive_page_two,
-  interactive_page_three,
+  interact_page_one,
+  interact_page_two,
+  interact_page_three,
   summary_page
 )
